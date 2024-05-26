c4d6043b537d30be92f78b84951bedded974eca4
dobroeytro <warm.citrus@gmail.com>
2024-05-26 18:29:11 +0300

    Deploy to GitHub Pages

--- src/app/sortMenuHandle.js
+++
@@ -1,6 +0,0 @@
-const sortButton = document.querySelector('.sort__button');
-const sortList = document.querySelector('.sort__list');
-
-sortButton.addEventListener('click', () => {
-    sortList.classList.toggle('visible');
-});
